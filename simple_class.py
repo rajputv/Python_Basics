2f3c31cec7f42cd327a52bad7f05423370e3da28
Vivek Rajput <rajputv.linux@gmail.com>
2016-05-20 21:14:44 -0700

    Created a simple class in python

---
+++ simple_class.py
@@ -0,0 +1,12 @@
+# Created a class called Greeter with 2 definitions
+class Greeter(object):
+	def hello(self):
+		print ("Hello")
+
+	def goodbye(self):
+		print("Goodbye")
+
+#instantiating the class
+g = Greeter()
+g.hello()
+g.goodbye()
